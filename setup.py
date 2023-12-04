79546ec4dbcfd42d5d71f4ef356bca45eb21f9f8
Carla Rodriguez <carlarodriguez.m96@gmail.com>
2023-12-04 11:52:04 +0100

    Updated requirements for installing xlumina

--- setup.py
+++ setup.py
@@ -33,12 +33,7 @@ setup(
     long_description=open('README.md').read() if exists('README.md') else '',
     long_description_content_type="text/markdown",
     install_requires=[
-        "jax==0.4.13",
-        "jaxlib==0.4.13=+cuda11.cudnn86"
-        "numpy>= 1.24.2",
-        "optax==0.1.7", 
-        "scipy==1.10.1", 
-        "matplotlib"
+        'jax==0.4.13', 'jaxlib==0.4.13', 'numpy', 'optax', 'scipy', 'matplotlib'
     ],
     python_requires=">=3.8",
     classifiers=['Operating System :: OS Independent',
