ad407adafddd8622761e3fefa6ab1ed5ab607141
Carla Rodriguez <carlarodriguez.m96@gmail.com>
2023-12-07 14:19:22 +0100

    Python files for light propagation testing in XLuminA.

---
+++ xlumina/examples/scalar_xlumina.py
@@ -0,0 +1,50 @@
+from xlumina.__init__ import mm
+from xlumina.wave_optics import *
+from xlumina.vectorized_optics import *
+from xlumina.optical_elements import *
+from xlumina.loss_functions import *
+from xlumina.toolbox import space
+import jax.numpy as jnp
+
+import time
+
+""" Computes the running times for scalar version of Rayleigh-Sommerfeld (RS) and Chirped z-transform (CZT) algorithms using XLuminA """
+
+# Light source settings
+resolution = 2048
+wavelength = .6328 * um
+w0 = (1200*um , 1200*um)
+x, y = space(15*mm, resolution)
+x_out, y_out = jnp.array(space(15*mm, resolution))
+
+gb = LightSource(x, y, wavelength)
+gb.gaussian_beam(w0=w0, E0=1)
+
+# Rayleigh-Sommerfeld:
+tic = time.perf_counter() 
+ls_propagated = gb.RS_propagation(z=5*mm)
+print("Time taken for 1st RS propagation - in seconds", time.perf_counter() - tic)
+
+time_RS_xlumina = []
+time_CZT_xlumina = []
+
+for i in range(101):
+    tic = time.perf_counter()
+    ls_propagated = gb.RS_propagation(z=5*mm)
+    t = time.perf_counter() - tic
+    time_RS_xlumina.append(t)
+
+    
+# Chirped z-transform:
+tic = time.perf_counter() 
+ls_propagated = gb.CZT(z=5*mm, xout=x_out, yout=y_out)
+print("Time taken for 1st CZT propagation - in seconds", time.perf_counter() - tic)
+
+for i in range(101):
+    tic = time.perf_counter()
+    ls_propagated = gb.CZT(z=5*mm, xout=x_out, yout=y_out)
+    t = time.perf_counter() - tic
+    time_CZT_xlumina.append(t)
+    
+filename = f"Scalar_propagation_xlumina_GPU.npy" 
+np.save(filename, {"RS_xlumina": time_RS_xlumina, "CZT_xlumina": time_CZT_xlumina})
---
+++ xlumina/examples/vectorial_xlumina.py
@@ -0,0 +1,47 @@
+from xlumina.__init__ import mm
+from xlumina.wave_optics import *
+from xlumina.vectorized_optics import *
+from xlumina.optical_elements import *
+from xlumina.loss_functions import *
+from xlumina.toolbox import space
+import jax.numpy as jnp
+
+import time
+
+""" Computes the running times for vectorial version of Rayleigh-Sommerfeld (VRS) and Chirped z-transform (VCZT) algorithms using XLuminA """
+
+# Light source settings
+resolution = 2048
+wavelength = .6328 * um
+w0 = (1200*um , 1200*um)
+x, y = space(15*mm, resolution)
+x_out, y_out = jnp.array(space(15*mm, resolution))
+
+gb_lp = PolarizedLightSource(x, y, wavelength)
+gb_lp.gaussian_beam(w0=w0, jones_vector=(1, 0))
+
+# Rayleigh-Sommerfeld:
+tic = time.perf_counter() 
+gb_propagated = gb_lp.VRS_propagation(z=5*mm)
+print("Time taken for 1st VRS propagation - in seconds", time.perf_counter() - tic)
+
+time_VRS_xlumina = []
+time_VCZT_xlumina = []
+
+for i in range(101):
+    tic = time.perf_counter()
+    gb_propagated = gb_lp.VRS_propagation(z=5*mm)
+    time_VRS_xlumina.append(time.perf_counter() - tic)
+    
+# Chirped z-transform:
+tic = time.perf_counter() 
+gb_propagated = gb_lp.VCZT(z=5*mm, xout=x_out, yout=y_out)
+print("Time taken for 1st VCZT propagation - in seconds", time.perf_counter() - tic)
+
+for i in range(101):
+    tic = time.perf_counter()
+    gb_propagated = gb_lp.VCZT(z=5*mm, xout=x_out, yout=y_out)
+    time_VCZT_xlumina.append(time.perf_counter() - tic)
+    
+filename = f"vectorial_propagation_xlumina_GPU.npy" 
+np.save(filename, {"VRS_xlumina": time_VRS_xlumina, "VCZT_xlumina": time_VCZT_xlumina})
