099848dc882313f15bb6ccb57e2a7154c8d64720
Carla Rodriguez <carlarodriguez.m96@gmail.com>
2024-10-07 17:18:10 +0200

    Updated requirements for installing xlumina

--- setup.py
+++ setup.py
@@ -14,31 +14,32 @@ package_name = 'xlumina'
 year = '2023'
 url = 'https://github.com/artificial-scientist-lab/XLuminA'
 
-
-def get_version():
-    content = open(Path(package_name) / '__init__.py').readlines()
-    return "0.0.1"
-
 setup(
     name=dist_name,
     author=author,
     author_email=email,
     url=url,
-    version=get_version(),
+    version="1.0.0",
     packages=find_packages(),
     package_dir={dist_name: package_name},
     include_package_data=True,
     license='MIT',
     description=description,
-    long_description=open('README.md').read() if exists('README.md') else '',
+    long_description=Path('README.md').read_text() if Path('README.md').exists() else '',
     long_description_content_type="text/markdown",
     install_requires=[
-        'jax==0.4.13', 'jaxlib==0.4.13', 'numpy', 'optax', 'scipy', 'matplotlib'
+        'jax==0.4.33',
+        'numpy',
+        'optax==0.2.3',
+        'scipy==1.14.1',
+        'matplotlib'
     ],
-    python_requires=">=3.8",
-    classifiers=['Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3',
-                 ],
+    python_requires=">=3.10",
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
     platforms=['ALL'],
     py_modules=[package_name],
 )
